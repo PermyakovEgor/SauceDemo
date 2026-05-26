bdddad08d9e762ad96d3c6ae45c100d0263ae722
Egor Permyakov <wot0110@yandex.ru>
2026-05-26 19:00:24 +0300

    Change level log

--- src/test/java/utils/TestListener.java
+++ src/test/java/utils/TestListener.java
@@ -13,7 +13,7 @@ public class TestListener implements ITestListener {
 
     @Override
     public void onTestStart(ITestResult iTestResult) {
-        log.info("======================================== STARTING TEST  '{}' ========================================%n", iTestResult.getName());
+        log.error("======================================== STARTING TEST  '{}' ========================================%n", iTestResult.getName());
     }
 
     @Override
